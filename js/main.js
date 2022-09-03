d077a9cfd3ff9c76e1ced89e1dece8b700376f8b
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2022-09-03 19:40:11 +0200

    Update of JS and HTML

--- js/main.js
+++ js/main.js
@@ -6,13 +6,58 @@ $("#datum").text(new Date().getDate() + "." + (new Date().getMonth() + 1) + "." 
 function mqResults() {
     var a = $('#odgovor').val();
 
-    if(a == "akcioni horor") {
+    if (a == "akcioni horor") {
         $("#rezultat").text("Odgovor je tačan.");
-        $("#rezultat").addClass("alert alert-success w-75 text-center mt-2");
+        $("#rezultat").addClass(" alert alert-success w-75 text-center mt-2");
     } else {
         $("#rezultat").html("Odgovor je netačan. <br> Upišite neki drugi odgovor (ili pronađite rješenje na Wikipediji ili IMDB-u).");
-        $("#rezultat").addClass("alert alert-danger w-75 text-center mt-2");
+        $("#rezultat").addClass(" alert alert-danger w-75 text-center mt-2");
     }
 
 }
-function provjeraOdgovora() { var a = document.forms[0].pitanje1.value, o = 0, t = 0; "BP" || "BL" == a ? o++ : t++, "Dvije" == document.forms[0].pitanje2.value ? o++ : t++, "Bilo koji" == document.forms[0].pitanje3.value ? o++ : t++, "Trece" == document.forms[0].pitanje4.value ? o++ : t++, "1999" == document.forms[0].pitanje5.value ? o++ : t++, "Tri" == document.forms[0].pitanje6.value ? o++ : t++, $("#rezultat").html("Broj tačnih odgovora: " + o + ", broj pogrešnih odgovora: " + t + "."), $("#rezultat").addClass("alert alert-info w-50 text-center mt-2 mx-auto") }
+
+function provjeraOdgovora() {
+    const form = document.forms[0];
+    var b = form.pitanje1.value;
+    var c = form.pitanje2.value;
+    var d = form.pitanje3.value;
+    var e = form.pitanje4.value;
+    var f = form.pitanje5.value;
+    var g = form.pitanje6.value;
+    var correct = 0;
+    var incorrect = 0;
+
+    if (b != "BGD") {
+        correct++
+    } else {
+        incorrect++
+    }
+    if (c == "Dvije") {
+        correct++
+    } else {
+        incorrect++
+    }
+    if (d == "Bilo koji") {
+        correct++
+    } else {
+        incorrect++
+    }
+    if (e == "Trece") {
+        correct++
+    } else {
+        incorrect++
+    }
+    if (f == "1999") {
+        correct++
+    } else {
+        incorrect++
+    }
+    if (g == "Tri") {
+        correct++
+    } else {
+        incorrect++
+    }
+
+    $("#rezultat").html("Broj tačnih odgovora: " + correct + ", broj pogrešnih odgovora: " + incorrect + ".");
+    $("#rezultat").addClass(" alert alert-info w-50 text-center mt-2 mx-auto");
+}
