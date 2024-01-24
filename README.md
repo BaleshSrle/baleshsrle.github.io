32775e9baf0e20f5098c8d8bd97a6d7a52ad5efa
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2024-01-24 17:52:13 +0100

    Update Of The Site
    Update of the sitemap, CV added, update of ReadMe, added RobotsTXT

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-[![](https://data.jsdelivr.com/v1/package/gh/BaleshSrle/ICBL_Project_Assignment_01/badge?style=rounded)](https://www.jsdelivr.com/package/gh/BaleshSrle/ICBL_Project_Assignment_01)
-[![Open in Visual Studio Code](https://img.shields.io/static/v1?logo=visualstudiocode&label=&message=Open%20in%20Visual%20Studio%20Code&labelColor=2c2c32&color=007acc&logoColor=007acc)](https://open.vscode.dev/BaleshSrle/ICBL_Project_Assignment_01)
-![Website](https://img.shields.io/website?url=https%3A%2F%2Fbaleshevich.dobojcaffe.com%2Fitp100%2F&logo=bootstrap&logoColor=white&labelColor=7952B3&cacheSeconds=120)
-![W3C Validation](https://img.shields.io/w3c-validation/html?targetUrl=https%3A%2F%2Fbaleshevich.dobojcaffe.com%2Fitp100%2F&cacheSeconds=120)
+[![](https://data.jsdelivr.com/v1/package/gh/BaleshSrle/baleshsrle.github.io/badge?style=rounded)](https://www.jsdelivr.com/package/gh/BaleshSrle/baleshsrle.github.io)
+[![Open in Visual Studio Code](https://img.shields.io/static/v1?logo=visualstudiocode&label=&message=Open%20in%20Visual%20Studio%20Code&labelColor=2c2c32&color=007acc&logoColor=007acc)](https://open.vscode.dev/BaleshSrle/baleshsrle.github.io)
+![Website](https://img.shields.io/website?url=https%3A%2F%2Fbaleshsrle.github.io%2F&logo=bootstrap&logoColor=white&labelColor=7952B3&cacheSeconds=120)
+![W3C Validation](https://img.shields.io/w3c-validation/html?targetUrl=https%3A%2F%2Fbaleshsrle.github.io%2F&cacheSeconds=120)
+[![pages-build-deployment](https://github.com/BaleshSrle/baleshsrle.github.io/actions/workflows/pages/pages-build-deployment/badge.svg?branch=main)](https://github.com/BaleshSrle/baleshsrle.github.io/actions/workflows/pages/pages-build-deployment)
 
 
 # ICBL_Project_Assignment_01
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $("html").attr("lang", navigator.language);
+    $("head").append("<link rel='dns-prefetch' href='https://cdn.jsdelivr.net/'>");
+    $("head").append("<link rel='preconnect' href='https://cdn.jsdelivr.net/'>");
     $("head").append("<link rel='icon' href='images/logo.png' type='image/png'>");
     $("head").append("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6/css/all.min.css'crossorigin='anonymous'>");
     $("body").addClass("d-flex flex-column h-100");
@@ -13,9 +15,9 @@ $(document).ready(function () {
     $("#lenovoThinkPadX230_link").attr("href", "https://mediamarket.rs.ba/index.php/laptopi/koristeni-laptopi/lenovothinkpadx230-434-detail");
     $("#fujitsuLifeBookS752_link").attr("href", "https://mediamarket.rs.ba/index.php/laptopi/koristeni-laptopi/fsc-lifebook-s752-detail");
     $("#fujitsuLifeBookS761_link").attr("href", "https://mediamarket.rs.ba/index.php/laptopi/koristeni-laptopi/fujitsulifebooks761-detail");
-    $("iframe.embed-responsive-item").addClass("border-0 rounded-lg").attr("allowfullscreen","true");
+    $("iframe.embed-responsive-item").addClass("border-0 rounded-lg").attr("allowfullscreen", "true");
     $("img.img-thumbnail").width(250);
-    $("[src*='laguna'],[src*='delfi'],[src*='carobnaknjiga'],[src*='samizdatb92'],[src*='cdnmpro'],[src*='vbz']").width(280).addClass("h-auto");
+    $("[src*='laguna'],[src*='delfi'],[src*='carobnaknjiga'],[src*='samizdatb92'],[src*='cdnmpro'],[alt$='Romanovima']").width(280).addClass("h-auto");
     $(".carousel").addClass("carousel-fade").carousel({
         interval: 3500,
         keyboard: false,
@@ -25,65 +27,60 @@ $(document).ready(function () {
     $("img[src*='slika']").addClass("d-block w-100 h-auto rounded-lg");
     $("footer").addClass("container-fluid bg-primary text-center text-white mt-auto mb-0 py-0 overflow-hidden fixed-bottom");
     $("#datum").text(new Date().getDate() + "." + (new Date().getMonth() + 1) + "." + new Date().getFullYear() + ".");
-
-    function mqResults() {
-        var a = $('#odgovor').val();
-
-        if (a == "akcioni horor") {
-            $("#rezultat").text("Odgovor je tačan.");
-            $("#rezultat").addClass(" alert alert-success w-75 text-center mt-2");
-        } else {
-            $("#rezultat").html("Odgovor je netačan. <br> Upišite neki drugi odgovor (ili pronađite rješenje na Wikipediji ili IMDB-u).");
-            $("#rezultat").addClass(" alert alert-danger w-75 text-center mt-2");
-        }
-
-    }
-
-    function provjeraOdgovora() {
-        const form = document.forms[0];
-        var b = form.pitanje1.value;
-        var c = form.pitanje2.value;
-        var d = form.pitanje3.value;
-        var e = form.pitanje4.value;
-        var f = form.pitanje5.value;
-        var g = form.pitanje6.value;
-        var correct = 0;
-        var incorrect = 0;
-
-        if (b != "BGD") {
-            correct++
-        } else {
-            incorrect++
-        }
-        if (c == "Dvije") {
-            correct++
-        } else {
-            incorrect++
-        }
-        if (d == "Bilo koji") {
-            correct++
-        } else {
-            incorrect++
-        }
-        if (e == "Trece") {
-            correct++
-        } else {
-            incorrect++
-        }
-        if (f == "1999") {
-            correct++
-        } else {
-            incorrect++
-        }
-        if (g == "Tri") {
-            correct++
-        } else {
-            incorrect++
-        }
-
-        $("#rezultat").html("Broj tačnih odgovora: " + correct + ", broj pogrešnih odgovora: " + incorrect + ".");
-        $("#rezultat").addClass(" alert alert-info w-50 text-center mt-2 mx-auto");
-    }
-
 });
 
+function mqResults() {
+    var a = $('#odgovor').val();
+
+    if (a == "akcioni horor") {
+        $("#rezultat").text("Odgovor je tačan.").addClass("alert alert-success w-75 text-center mt-2");
+    } else {
+        $("#rezultat").html("Odgovor je netačan. <br> Upišite neki drugi odgovor (ili pronađite rješenje na Wikipediji ili IMDb-u).").addClass("alert alert-danger w-75 text-center mt-2");
+    }
+}
+
+function provjeraOdgovora() {
+    const form = document.forms[0];
+    var b = form.pitanje1.value;
+    var c = form.pitanje2.value;
+    var d = form.pitanje3.value;
+    var e = form.pitanje4.value;
+    var f = form.pitanje5.value;
+    var g = form.pitanje6.value;
+    var correct = 0;
+    var incorrect = 0;
+
+    if (b != "BGD") {
+        correct++
+    } else {
+        incorrect++
+    }
+    if (c == "Dvije") {
+        correct++
+    } else {
+        incorrect++
+    }
+    if (d == "Bilo koji") {
+        correct++
+    } else {
+        incorrect++
+    }
+    if (e == "Trece") {
+        correct++
+    } else {
+        incorrect++
+    }
+    if (f == "1999") {
+        correct++
+    } else {
+        incorrect++
+    }
+    if (g == "Tri") {
+        correct++
+    } else {
+        incorrect++
+    }
+
+    $("#rezultat").html("Broj tačnih odgovora: " + correct + ", broj pogrešnih odgovora: " + incorrect + ".");
+    $("#rezultat").addClass(" alert alert-info w-50 text-center mt-2 mx-auto");
+}
