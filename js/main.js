41dc80a9b60069c7a65c2e329539b06406f68cf6
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2024-09-30 21:27:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,25 +4,29 @@ $(document).ready(function () {
     if (BrowserLang.includes("sr") || BrowserLang.includes("hr") || BrowserLang.includes("bs")) {
         $("[lang='sr']").show(1);
         $("[lang='en']").hide(1);
-        $("li.nav-item").eq(0).find("span").text("Početna");
-        $("li.nav-item").eq(1).find("span").text("Rođendanski poklon");
-        $("li.nav-item").eq(2).find("span").text("Omiljeni film");
-        $("li.nav-item").eq(3).find("span").text("Omiljeni grad");
-        $("li.nav-item").eq(4).find("span").text("Hobi");
-        $("li.nav-item").eq(5).find("span").text("Galerija");
-        $("li.nav-item").eq(6).find("span").text("Kviz");
-        $("li.nav-item").eq(7).find("span").text("Mapa sajta");
+        $("li.nav-item").each(function () {
+            $(this).eq(0).find("span").text("Početna");
+            $(this).eq(1).find("span").text("Rođendanski poklon");
+            $(this).eq(2).find("span").text("Omiljeni film");
+            $(this).eq(3).find("span").text("Omiljeni grad");
+            $(this).eq(4).find("span").text("Hobi");
+            $(this).eq(5).find("span").text("Galerija");
+            $(this).eq(6).find("span").text("Kviz");
+            $(this).eq(7).find("span").text("Mapa sajta");
+        });
     } else {
         $("[lang='en']").show(1);
         $("[lang='sr']").hide(1);
-        $("li.nav-item").eq(0).find("span").text("Home");
-        $("li.nav-item").eq(1).find("span").text("Birthday present");
-        $("li.nav-item").eq(2).find("span").text("Favourite movie");
-        $("li.nav-item").eq(3).find("span").text("Favourite town");
-        $("li.nav-item").eq(4).find("span").text("Hobby");
-        $("li.nav-item").eq(5).find("span").text("Gallery");
-        $("li.nav-item").eq(6).find("span").text("Quiz");
-        $("li.nav-item").eq(7).find("span").text("Site Map");
+        $("li.nav-item").each(function () {
+            $(this).eq(0).find("span").text("Home");
+            $(this).eq(1).find("span").text("Birthday present");
+            $(this).eq(2).find("span").text("Favourite movie");
+            $(this).eq(3).find("span").text("Favourite town");
+            $(this).eq(4).find("span").text("Hobby");
+            $(this).eq(5).find("span").text("Gallery");
+            $(this).eq(6).find("span").text("Quiz");
+            $(this).eq(7).find("span").text("Site Map");
+        });
     }
     switch (location.pathname) {
         case "/birthdaypresent.html":
@@ -34,16 +38,20 @@ $(document).ready(function () {
             $("head").prepend("<meta name='google-site-verification' content='HcgjQ9u13iaWAG1FyF9yFp6-f9QYrwstKoKPNrRbbqM'>", "<meta name='google-site-verification' content='YXXACXsZjuu041kl41yKLww4xdiiudN1S0IEtdEeGSE'>", "<meta name='msvalidate.01' content='6605C60582C09EBF05D19C7AA9FF5496'>", "<meta name='msvalidate.01' content='78FE17549C9303A86665F2ECA50117F0'>", "<meta name='wot-verification' content='cee6cf583ee362cd666b'>", "<meta name='norton-safeweb-site-verification' content='AE1KUNZ8BFEIRXCIVRMN7804CZR-WK4ER0C3CPJO8W9N0ZVGHX9R2BJ4RNAG2DCL6SBHR6U7BDTD9VH3JKZG3FLO1EGE5E3CVLLQO4E1FM13PDFWE0RX9GUQ8SJWKLEW'>");
         //$("head").prepend("<meta http-equiv='Content-Security-Policy' content='default-src https:'>");
     }
-    $("head").append("<meta name='author' content='Baleševi&#263; Sr&#273;an, srdjan.b269%40gmail.com'>", "<meta name='designer' content='BaleshSrle'>", "<meta name='reply-to' content='srdjan.balesevic%40yahoo.com'>", "<meta name='language' content='sr'>", "<meta name='host' content='pages.github.com'>", "<meta name='geo.region' content='BA-SRP'>", "<meta name='geo.placename' content='Добој'>", "<meta name='twitter:creator' content='@BaleshSrle'>", "<meta name='twitter:site' content='@BaleshSrle'>");
-    $("head").append("<meta name='MSThemeCompatible' content='yes'>", "<meta http-equiv='cleartype' content='on'>", "<meta http-equiv='X-UA-Compatible' content='ie=edge'>", "<meta http-equiv='x-dns-prefetch-control' content='on'>", "<meta http-equiv='Cache-control' content='no-cache'>", "<meta http-equiv='Pragma' content='no-cache'>", "<meta http-equiv='Expires' content='-1'>");
-    $("head").append("<link rel='dns-prefetch' href='https://cdn.jsdelivr.net/'>", "<link rel='preconnect' href='https://cdn.jsdelivr.net/'>", "<link rel='icon' href='images/logo.png' type='image/png'>", "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6/css/all.min.css'>", "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/flag-icons/css/flag-icons.min.css'>");
-    $("[rel~='preconnect'],[rel~='stylesheet'],[src$='jquery.slim.min.js'],[src$='bootstrap.bundle.min.js'],[src$='jquery.typewriter.min.js'],[src$='jquery.countdown.min.js']").attr("crossorigin", "anonymous");
+    $("head").each(function () {
+        $(this).append("<meta name='author' content='Baleševi&#263; Sr&#273;an, srdjan.b269%40gmail.com'>", "<meta name='designer' content='BaleshSrle'>", "<meta name='reply-to' content='srdjan.balesevic%40yahoo.com'>", "<meta name='language' content='sr'>", "<meta name='host' content='pages.github.com'>", "<meta name='geo.region' content='BA-SRP'>", "<meta name='geo.placename' content='Добој'>", "<meta name='twitter:creator' content='@BaleshSrle'>", "<meta name='twitter:site' content='@BaleshSrle'>");
+        $(this).append("<meta name='MSThemeCompatible' content='yes'>", "<meta http-equiv='cleartype' content='on'>", "<meta http-equiv='X-UA-Compatible' content='ie=edge'>", "<meta http-equiv='x-dns-prefetch-control' content='on'>", "<meta http-equiv='Cache-control' content='no-cache'>", "<meta http-equiv='Pragma' content='no-cache'>", "<meta http-equiv='Expires' content='-1'>");
+        $(this).append("<link rel='dns-prefetch' href='https://cdn.jsdelivr.net/'>", "<link rel='preconnect' href='https://cdn.jsdelivr.net/'>", "<link rel='icon' href='images/logo.png' type='image/png'>", "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6/css/all.min.css'>", "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/flag-icons/css/flag-icons.min.css'>");
+    });
+    $("[rel~='preconnect'],[rel~='stylesheet'],[src$='jquery.min.js'],[src$='bootstrap.bundle.min.js'],[src$='jquery.typewriter.min.js'],[src$='jquery.countdown.min.js']").attr("crossorigin", "anonymous");
     $("body").addClass("d-flex flex-column h-100");
     $("main").addClass("flex-shrink-0 mb-3 pb-5");
-    $("header.jumbotron.jumbotron-fluid").addClass("mb-0 py-0 overflow-hidden");
-    $("header").find("div.col-sm-2").eq(0).addClass("order-1 order-md-0");
-    $("header").find("div.col-sm-9").addClass("py-md-4 order-2 order-md-1");
-    $("header").find("div.col-sm-1").addClass("order-0 order-md-2");
+    $("header").each(function () {
+        $(this).filter(".jumbotron.jumbotron-fluid").addClass("mb-0 py-0 overflow-hidden");
+        $(this).find("div.col-sm-2").eq(0).addClass("order-1 order-md-0");
+        $(this).find("div.col-sm-9").addClass("py-md-4 order-2 order-md-1");
+        $(this).find("div.col-sm-1").addClass("order-0 order-md-2");
+    });
     $("span.fi").addClass("float-right m-1 border border-warning");
     $("li.nav-item").find("i").addClass("fa-lg pr-2");
     $("div[class*='container']").addClass("py-2 mx-auto");
